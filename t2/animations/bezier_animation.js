1c12649790345f95d8300ca17ef822667c97ceac
Pedro <pedro21fcp@gmail.com>
2017-11-21 12:33:23 +0000

    update bezier
    Correct I guess

--- t2/animations/bezier_animation.js
+++ t2/animations/bezier_animation.js
@@ -2,7 +2,7 @@ class BezierAnimation extens Animation{
   constructor(scene,args){
     super(scene,args[0],args[1]);
     this.control_points=args[2];
-    this.velocity = args[2];
+    this.velocity = args[1];
     this.p1 = this.control_points[0];
     this.p2 = this.control_points[1];
     this.p3 = this.control_points[2];
@@ -19,15 +19,41 @@ class BezierAnimation extens Animation{
     this.p4x= this.p4[0];
     this.p4y= this.p4[1];
     this.p4z= this.p4[2];
-    this.distance = distance_between_points(this.p1,this.p4);
-    this.time=this.distance/this.velocity;
+    this.distance = bezier_distance(this.control_points);
     this.t=0;
     this.lastUpdate=-1;
-    this.angulo = 1; // = ao angulo inicial do carro
+    this.angulo = 0;
   }
 
   function distance_between_points(p1,p2){
     return Math.sqrt(Math.pow((p2[0]-p1[0]),2)+Math.pow((p2[1]-p1[1]),2)+Math.pow((p2[2]-p1[2]),2));
+  }
+
+  function bezier_distance(c_pts)
+  {
+    var p1=ponto_medio(c_pts[0],c_pts[1]);          //ponto entre P1 e P2
+
+    var p2=ponto_medio(c_pts[1],c_pts[2]);          //ponto entre P2 e P3
+
+    var p3=ponto_medio(c_pts[2],c_pts[3]);          //ponto entre P3 e P4
+
+    var p4=ponto_medio(p1,p2);                     //ponto entre p1 e p2(criados acima)
+
+    var p5=ponto_medio(p2,p3);                    //ponto entre p2 e p3(criados acima)
+
+
+    return distance_between_points(c_pts[0],p1) + distance_between_points(p1,p4) + distance_between_points(p4,p5) + distance_between_points(p5,c_pts[3]);
+
+  }
+
+
+  function ponto_medio(p1,p2){
+
+    var x =(p1[0]+p2[0])/2
+    var y =(p1[1]+p2[1])/2
+    var p=[x,y]
+    return p;
+
   }
 
   update(){
@@ -35,18 +61,15 @@ class BezierAnimation extens Animation{
   if (this.lastUpdate == -1)
   {
 		this.lastUpdate = currTime;
-		inc = 0;
 	}
 	else
   {
 		var diff = currTime - this.lastUpdate;
 		this.lastUpdate = currTime;
-		inc = diff * this.velocity/(this.distance*1000);
+		this.t=(this.velocity*diff)/this.distance;
 	}
   else
   {
-
-		this.t+=inc;
     var P=[this.x, this.y, this.z];
 		this.x=Math.pow((1-this.t),3)*this.p1x + 3*this.t*Math.pow((1-this.t),2)*this.p2x+3*Math.pow(this.t,2)*(1-this.t)*this.p3x+Math.pow(this.t,3)*this.p4x;
 		this.y=Math.pow((1-this.t),3)*this.p1y + 3*this.t*Math.pow((1-this.t),2)*this.p2y+3*Math.pow(this.t,2)*(1-this.t)*this.p3y+Math.pow(this.t,3)*this.p4y;
